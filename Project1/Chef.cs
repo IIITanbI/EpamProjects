ac11e4be2a8c2f94399723ef5b6824b35652b3df
paladinama <paladinama@tut.by>
2015-10-19 00:56:49 +0300

    lst

---
+++ Project1/Chef.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class Chef
+    {
+        public string Name { get; } = "qwerty";
+        public Salad Salad { get; }
+
+        public Chef()
+        {
+            
+        }
+        public Chef(string name)
+        {
+            Name = name;
+        }
+        public Chef(string name, Salad salad)
+            : this(name)
+        {
+            Salad = salad;
+        }
+
+        
+    }
+}
